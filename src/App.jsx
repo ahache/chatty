9db6d4222675025148144ab4443352bde71438c1
Alex Hache <ahache8@gmail.com>
2017-08-23 00:20:30 +0000

    Setup message communication between App and Chatbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar.jsx';
-import Messages from './Messages.jsx';
+import MessageList from './MessageList.jsx';
 import Chatbar from './Chatbar.jsx';
 
+const data = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentUser: data.currentUser.name,
+      messages: data.messages
+    }
+    this.onNewPost = this.onNewPost.bind(this);
+  }
+
+  onNewPost(post) {
+    const newMessage = {username: post.user, content: post.content};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages});
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <Messages />
-        <Chatbar />
+        <MessageList messages={ this.state.messages } />
+        <Chatbar user={ this.state.currentUser } onNewPost={ this.onNewPost } />
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 
 class Chatbar extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      content: '',
+      user: 'Anonymous'
+    }
+  }
+
+  getUser = (event) => {
+    this.setState({user: event.target.value});
+  }
+
+  handleInput = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({content: event.target.value}, function() {
+        this.props.onNewPost(this.state);
+      });
+    }
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onKeyUp={this.getUser} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.handleInput} />
       </footer>
     );
   }
